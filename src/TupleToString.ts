46d2060efa1d96e5314323ca4cd7742fc3672332
Austin Theriot <austinmtheriot@gmail.com>
2021-05-28 05:36:13 -0500

    format with prettier

--- src/TupleToString.ts
+++ src/TupleToString.ts
@@ -1,23 +1,23 @@
-import { Test } from "ts-toolbelt"
-import { MakeIndexes } from "./MakeIndexes"
-import { RemoveHead } from "./RemoveHead"
+import { Test } from 'ts-toolbelt';
+import { MakeIndexes } from './MakeIndexes';
+import { RemoveHead } from './RemoveHead';
 
-const { checks, check } = Test
+const { checks, check } = Test;
 
 /**
  * Removes the first element from a tuple.
  */
 export type TupleToString<
-  Tuple extends string[],
-  _StorageString extends string = '',
-  > = Tuple extends string[] ? (Tuple['length'] extends 0
-    ? _StorageString : TupleToString<
-      RemoveHead<Tuple>,
-      `${_StorageString}${Tuple[0]}`
-    >) : never;
+	Tuple extends string[],
+	_StorageString extends string = ''
+> = Tuple extends string[]
+	? Tuple['length'] extends 0
+		? _StorageString
+		: TupleToString<RemoveHead<Tuple>, `${_StorageString}${Tuple[0]}`>
+	: never;
 
 checks([
-  // should concatenate a tuple of strings into a single string
-  check<TupleToString<['hello ', 'world']>, 'hello world', Test.Pass>(),
-  check<TupleToString<[]>, '', Test.Pass>(),
-])
+	// should concatenate a tuple of strings into a single string
+	check<TupleToString<['hello ', 'world']>, 'hello world', Test.Pass>(),
+	check<TupleToString<[]>, '', Test.Pass>(),
+]);
