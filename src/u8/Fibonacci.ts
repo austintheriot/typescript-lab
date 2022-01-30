9acba9f3bae604993b07307782670c429fb2750a
Austin Theriot <austinmtheriot@gmail.com>
2022-01-29 22:13:43 -0600

    implement Fibonacci

---
+++ src/u8/Fibonacci.ts
@@ -0,0 +1,25 @@
+import { Test } from 'ts-toolbelt';
+import { Add } from "./Add";
+import { Dec } from "./Dec";
+const { checks, check } = Test;
+
+type _Fibonacci<A, B, N = 0> = N extends 0
+  ? A
+  : _Fibonacci<B, Add<A, B>, Dec<N>>;
+
+type Fibonacci<N = 0> = _Fibonacci<0, 1, N>;
+
+
+checks([
+  check<Fibonacci<0>, 0, Test.Pass>(),
+  check<Fibonacci<1>, 1, Test.Pass>(),
+  check<Fibonacci<2>, 1, Test.Pass>(),
+  check<Fibonacci<3>, 2, Test.Pass>(),
+  check<Fibonacci<4>, 3, Test.Pass>(),
+  check<Fibonacci<5>, 5, Test.Pass>(),
+  check<Fibonacci<6>, 8, Test.Pass>(),
+  check<Fibonacci<7>, 13, Test.Pass>(),
+  check<Fibonacci<8>, 21, Test.Pass>(),
+  check<Fibonacci<9>, 34, Test.Pass>(),
+  check<Fibonacci<10>, 55, Test.Pass>(),
+]);
